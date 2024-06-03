86c34b07d5f83e56031ce6c158ee551c40009bb9
Zniver4 <cdmx2592@amerike.edu.mx>
2024-06-03 13:03:56 -0600

    Github pages Bug fix
    Yipeeeeeeeeee

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,31 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("code/css");
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 return {
+  
     dir: {
+      
       input: "code",
       output: "docs",
     }
   };
 }
+
+
+
+/*
+module.exports = function (eleventyConfig) {
+	eleventyConfig.setQuietMode(true);
+};
+
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
+	return {
+		pathPrefix: "/WikiTerraria/",
+	};
+};
+*/
